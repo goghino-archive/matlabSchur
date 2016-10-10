c42c9e5d19b62cd825f38099e046734d96ffe88b
Juraj Kardos <juraj.kardos@usi.ch>
2016-10-10 16:03:43 +0200

    adding np parameter

--- interface.m
+++ interface.m
@@ -1,5 +1,10 @@
+% Optimal control system
 A = '/home/kardos/block_solve/matrices/PardisoMat_10_2.csr';
+% Grid size
 N = 10;
+% Number of scenarios
 NS = 2;
+%number of worker processes
+np = 1;
 
-X = mexsolve(A, N, NS);
+X = mexsolve(A, N, NS, np);
